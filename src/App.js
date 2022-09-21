826f4fc5922e3d86c3084612ae74c4c67cf17e31
iamjayenmthakuri <malla.jayen@gmail.com>
2022-09-21 11:42:16 +0545

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Profile from "./component/Profile";
+import Login from "./component/Login";
+import ChangeColor from "./component/ChangeColor";
+
+function App() {
+  return (
+    <div className="App">
+      <Profile />
+      <Login />
+      <ChangeColor />
+    </div>
+  );
+}
+
+export default App;
